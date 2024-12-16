2ad3405fe4b2535966da9a0f91be5bc654cb121f
kamalesh <kamalikamalesh0401@gmail.com>
2024-12-16 16:57:13 +0530

    16-Dec-2024 bug fix

--- server.js
+++ server.js
@@ -9,10 +9,17 @@ app.use(express.json());
 app.use(cors());
 
 // MongoDB Connection
-mongoose.connect('mongodb+srv://TestUser:Test0401@cluster0.eqlho.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://TestUser:Test0401@cluster0.eqlho.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 30000 })
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.error(err));
 
+mongoose.connection.on("error", (err) => {
+    console.error("Database connection error:", err.message);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Database connection lost");
+});
 // Routes
 app.use('/api/fuel', require('./routes/fuelSensorRoutes'));
 
